3638ebe77d18003a1909aaec5df904b702b5a49a
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-06-07 22:42:45 -0500

    rest of the api calls from arrays added to films

--- src/pages/moreInfoFilms.js
+++ src/pages/moreInfoFilms.js
@@ -6,14 +6,17 @@ export default function MoreInfoFilm() {
   const { id } = useParams();
   const [film, setFilm] = useState([]);
   const [people, setPeople] = useState([{'id':1, 'name': 'No characteres can be named currently'}]);
-  const [location, setLocation] = useState([]);
-  const [species, setSpecies] = useState([]);
-  const [vehicles, setVehicles] = useState([]);
+  const [location, setLocation] = useState([{'id':1, 'name': 'No locations can be named currently'}]);
+  const [species, setSpecies] = useState([{'id':1, 'name': 'No species can be named currently'}]);
+  const [vehicles, setVehicles] = useState([{'id':1, 'name': 'No vehicles can be named currently'}]);
 
   useEffect(() =>{
     getFilmsID(id).then((event) =>{
       setFilm(event);
       let changedData = [];
+      let changedData1 = [];
+      let changedData2 = [];
+      let changedData3 = [];
       if(event.people[0] !== 'https://ghibliapi.herokuapp.com/people/'){
         Promise.all(event.people.map((person) => {
           return getExtra(person).then((data) => {
@@ -23,9 +26,37 @@ export default function MoreInfoFilm() {
           setPeople(changedData);
         });
       }
+      if(event.locations[0] !== 'https://ghibliapi.herokuapp.com/locations/'){
+        Promise.all(event.locations.map((location) => {
+          return getExtra(location).then((data) => {
+            changedData1.push(data);
+          });
+        })).then(() => {
+          setLocation(changedData1);
+        });
+      }
+      if(event.species[0] !== 'https://ghibliapi.herokuapp.com/species/'){
+        Promise.all(event.species.map((specie) => {
+          return getExtra(specie).then((data) => {
+            changedData2.push(data);
+          });
+        })).then(() => {
+          setSpecies(changedData2);
+        });
+      }
+      if(event.vehicles[0] !== 'https://ghibliapi.herokuapp.com/vehicles/'){
+        Promise.all(event.vehicles.map((vehicle) => {
+          return getExtra(vehicle).then((data) => {
+            changedData3.push(data);
+          });
+        })).then(() => {
+          setVehicles(changedData3);
+        });
+      }
     })
   }, [id]);
 
+  console.log(vehicles)
   return (
     <div>
       <a href='/'>
@@ -33,7 +64,16 @@ export default function MoreInfoFilm() {
       </a>
        {people.map((person) =>(
         <p key={person.id}>{person.name}</p>
+      ))}
+      {location.map((location) =>(
+        <p key={location.id}>{location.name}</p>
       ))} 
+      {species.map((specie) =>(
+        <p key={specie.id}>{specie.name}</p>
+      ))} 
+      {vehicles.map((vehicle) =>(
+        <p key={vehicle.id}>{vehicle.name}</p>
+      ))}  
       <p>Title:</p>
       <p>{film.title}</p>
       <p>Original Title:</p>
