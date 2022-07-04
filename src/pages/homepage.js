44bd000bd41be283db596a5a0e4ef98007b17c35
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-04 09:38:15 -0500

    tabs added to homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -4,12 +4,32 @@ import React, {useEffect, useState} from 'react';
 import HomeCards from '../components/homeCards';
 import HomeFilm from '../components/homeFilm';
 
+const tabs =[
+  {
+    id: '1',
+    name: 'Films',
+  }, {
+    id: '2',
+    name: 'Characters',
+  }, {
+    id: '3',
+    name: 'Locations',
+  }, {
+    id: '4',
+    name: 'Species',
+  }, {
+    id: '5',
+    name: 'Vehicle',
+  }
+]
+
 export function Homepage() {
   const [films, setFilms] = useState([]);
   const [people, setPeople] = useState([]);
   const [location, setLocation] = useState([]);
   const [species, setSpecies] = useState([]);
   const [vehicles, setVehicles] = useState([]);
+  const [tabActive, setTabActive] = useState('Films');
 
   useEffect(() => {
     getFilms().then((events) => {
@@ -29,29 +49,42 @@ export function Homepage() {
     });
   }, []);
 
+  function tabButton(event, name) {
+    event.preventDefault();
+    setTabActive(name);
+  }
+
   return (
     <div>
-      <div className='grid-container-film'>
+      <div className='centerLine'>
+        {tabs.map((tab) => (
+          <span key={tab.id}>
+            {tabActive === tab.name
+              ? <button className='tab-buttons-active' id='tab-buttons' onClick={(e) => tabButton(e, tab.name)}>{tab.name}</button>
+              : <button id='tab-buttons' onClick={(e) => tabButton(e, tab.name)}>{tab.name}</button>
+            }
+          </span>
+        ))}
+      </div>
+
+      <div className='grid-container-film' style={tabActive === 'Films' ? {} : {display: 'none'}}>
         <HomeFilm films={films}/>
       </div>
 
-      <h3>Ghibli characters (humans and non humans)</h3>
-      <div className='grid-container'>
+      
+      <div className='grid-container' style={tabActive === 'Characters' ? {} : {display: 'none'}}>
         <HomeCards people={people}/>
       </div>
 
-      <h3>Locations ( lands, countries, and places)</h3>
-      <div className='grid-container'>
+      <div className='grid-container' style={tabActive === 'Locations' ? {} : {display: 'none'}}>
         <HomeCards location={location}/>
       </div>
 
-      <h3>Species (humans, animals, and spirits...)</h3>
-      <div className='grid-container'>
+      <div className='grid-container' style={tabActive === 'Species' ? {} : {display: 'none'}}>
         <HomeCards species={species}/>
       </div>
 
-      <h3>Vehicle</h3>
-      <div className='grid-container'>
+      <div className='grid-container' style={tabActive === 'Vehicle' ? {} : {display: 'none'}}>
         <HomeCards vehicles={vehicles}/>
       </div>
     </div>
