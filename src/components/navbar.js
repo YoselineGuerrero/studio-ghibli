eb173a433be3ba631a2cdc55f08b3a23d0b3d1a0
yoseline guerrero <76671651+YoselineGuerrero@users.noreply.github.com>
2022-07-05 16:50:15 -0500

    clean ups on homepage

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <p className='center size-title font-large'>Studio Ghibli</p>
+      <p className='center font-large' style={{marginBottom: '0'}}>Studio Ghibli</p>
     </nav>
   );
 }
